30e54f530fe703796d15fbeeac6c2eadaa98721c
Tsy_Xx~ <13135792+tsy-xx@user.noreply.gitee.com>
2024-05-08 22:07:06 +0800

    尝试连接openGauss，失败

--- src/main/java/com/example/Kexie/config/MPConfig.java
+++ src/main/java/com/example/Kexie/config/MPConfig.java
@@ -1,5 +1,6 @@
 package com.example.Kexie.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
             //1.定义Mp拦截器
             MybatisPlusInterceptor mpInterceptor = new MybatisPlusInterceptor();
             //2.添加具体的拦截器
-            mpInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+            mpInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
             return mpInterceptor;
         }
     }
--- src/test/java/com/example/Kexie/DemoApplicationTests.java
+++ src/test/java/com/example/Kexie/DemoApplicationTests.java
@@ -25,35 +25,17 @@ import java.sql.Time;
 class DemoApplicationTests {
     @Autowired
     private Word_userDao word_userDao;
+    @Autowired
+    private UserDao userDao;
 
     @Test
-    private void l(){
-//        try {
-//            //创建工作簿对象
-//            XSSFWorkbook xssfWorkbook = new XSSFWorkbook(new FileInputStream("E://java17//java_practise//poiReadExcel//src//main//resources//chart.xlsx"));
-//            //获取工作簿下sheet的个数
-//            int sheetNum = xssfWorkbook.getNumberOfSheets();
-//            System.out.println("该excel文件中总共有："+sheetNum+"个sheet");
-//            //遍历工作簿中的所有数据
-//            for(int i = 0;i<sheetNum;i++) {
-//                //读取第i个工作表
-//                System.out.println("读取第"+(i+1)+"个sheet");
-//                XSSFSheet sheet = xssfWorkbook.getSheetAt(i);
-//                //获取最后一行的num，即总行数。此处从0开始
-//                int maxRow = sheet.getLastRowNum();
-//                for (int row = 0; row <= maxRow; row++) {
-//                    //获取最后单元格num，即总单元格数 ***注意：此处从1开始计数***
-//                    int maxRol = sheet.getRow(row).getLastCellNum();
-//                    System.out.println("--------第" + row + "行的数据如下--------");
-//                    for (int rol = 0; rol < maxRol; rol++){
-//                        System.out.print(sheet.getRow(row).getCell(rol) + "  ");
-//                    }
-//                    System.out.println();
-//                }
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+    public void l(){
+        System.out.println(1);
+        System.out.println(userDao.selectById(3));
+    }
+    @Test
+    public void print(){
+
+        System.out.println(1);
     }
 }
